8e9e38b84f4e7b294e434bc00d54b301b4cdb56c
mishyTheBear <hamish.dickson@gmail.com>
2014-12-06 14:57:03 +0000

    Add in size to starbuzz coffee

--- src/main/java/com/hamish/decorator/beverages/Beverage.java
+++ src/main/java/com/hamish/decorator/beverages/Beverage.java
@@ -4,10 +4,22 @@ package com.hamish.decorator.beverages;
  * Created by hamishdickson on 06/12/14.
  */
 public abstract class Beverage {
+    // These bit's didn't originally exist (see last commit)
+    public enum Size {TALL, GRANDE, VENTI};
+    Size size = Size.TALL;
+
     String description = "Unknown Beverage";
 
     public String getDescription() {
         return description;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
     }
 
     public abstract double cost();
