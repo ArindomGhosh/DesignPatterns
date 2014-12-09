2d9bbd275b6ea13cedaa783f877cde4f36375c8e
mishyTheBear <hamish.dickson@gmail.com>
2014-12-09 19:20:48 +0000

    Add garage door to simple command pattern

---
+++ src/main/java/com/hamish/command/simple/GarageDoor.java
@@ -0,0 +1,11 @@
+package com.hamish.command.simple;
+
+/**
+ * Created by hamishdickson on 09/12/14.
+ */
+public class GarageDoor {
+
+    public void open() {
+        System.out.println("Garage door open!");
+    }
+}
--- src/main/java/com/hamish/command/simple/RemoteControlTest.java
+++ src/main/java/com/hamish/command/simple/RemoteControlTest.java
@@ -7,9 +7,14 @@ public class RemoteControlTest {
     public static void main(String[] args) {
         SimpleRemoteControl remoteControl = new SimpleRemoteControl();
         Light light = new Light();
+        GarageDoor garageDoor = new GarageDoor();
         LightCommand lightOn = new LightCommand(light);
+        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
 
         remoteControl.setCommand(lightOn);
+        remoteControl.buttonWasPressed();
+
+        remoteControl.setCommand(garageDoorOpenCommand);
         remoteControl.buttonWasPressed();
     }
 }
