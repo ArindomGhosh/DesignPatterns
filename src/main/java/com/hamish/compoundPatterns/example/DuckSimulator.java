be7bb6c0daca3d0a5e819649e0670ca16d3f422e
mishyTheBear <hamish.dickson@gmail.com>
2015-01-05 09:31:37 +0000

    add decorator to compound pattern

--- src/main/java/com/hamish/compoundPatterns/example/DuckSimulator.java
+++ src/main/java/com/hamish/compoundPatterns/example/DuckSimulator.java
@@ -17,6 +17,14 @@ public class DuckSimulator {
         // make a goose that acts like a ducky
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
+        // could have used the original, but I'd like to keep it there for ref
+        Quackable mallardDuckQuack = new QuackCounter(new MallardDuck());
+        Quackable redheadDuckQuack = new QuackCounter(new ReadheadDuck());
+        Quackable duckCallQuack = new QuackCounter(new DuckCall());
+        Quackable rubberDuckQuack = new QuackCounter(new RubberDuck());
+        // let's not care about the goose!
+
+
         System.out.println("\nDuck simulator");
 
         simulate(mallardDuck);
@@ -25,6 +33,13 @@ public class DuckSimulator {
         simulate(rubberDuck);
         // once it's wrapped, can treat it like a duck!
         simulate(gooseDuck);
+
+        simulate(mallardDuckQuack);
+        simulate(redheadDuckQuack);
+        simulate(duckCallQuack);
+        simulate(rubberDuckQuack);
+
+        System.out.println("The ducks quacked " + QuackCounter.getNumberOfQuacks() + " times");
     }
 
     private void simulate(Quackable duck) {
